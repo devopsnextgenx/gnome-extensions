d262eed52ffb29e3965d4c78ab471b341388592d
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2024-11-10 22:04:26 -0500

    working clipboard and status message

--- gui-react-ollama/src/components/common/message/code.tsx
+++ gui-react-ollama/src/components/common/message/code.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { FaRegCopy, FaRegClipboard } from 'react-icons/fa';
+import "./code.css";
 
 function uuidv4() {
     return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
@@ -18,19 +20,18 @@ export interface CodeBlockProps {
 }
 
 export const CodeBlock = ({ language, code, style = atomDark }: CodeBlockProps) => {
-    const [copied, setCopied] = useState(false);
-
-    const handleCopy = () => {
-        setCopied(true);
-        setTimeout(() => setCopied(false), 1500); // Reset after 1.5 seconds
-    };
+    const [isCopied, setIsCopied] = useState(false);
 
     return (
-        <div key={uuidv4()}>
-            <CopyToClipboard text={code} onCopy={handleCopy}>
-                <button>{copied ? "Copied!" : "Copy"}</button>
+        <div key={uuidv4()} className="parentDiv">
+            <CopyToClipboard
+                text={code}
+                onCopy={() => setIsCopied(true)}
+                >
+                <button type="button" className="copy-button" aria-label="Copy to Clipboard Button">
+                    {isCopied ? <FaRegClipboard /> : <FaRegCopy />}
+                </button>
             </CopyToClipboard>
-
             <SyntaxHighlighter language={language} style={style}>
                 {code}
             </SyntaxHighlighter>
